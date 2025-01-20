1d548a7720a8ef63dea87dd98fa083d36ce055ad
求饶 <163741770+BaiXingshuang@users.noreply.github.com>
2025-01-20 23:18:58 +0800

    修改代封面
    修改代封面
    
    Co-Authored-By: Mu_yunbai <114895633+zhangyuxinn@users.noreply.github.com>
    Co-Authored-By: Yuan0x1 <143624539+Yuan0x1@users.noreply.github.com>

---
+++ Assets/Scripts/UI/HorizontalCardHolder.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using DG.Tweening;
+using System.Linq;
+
+public class HorizontalCardHolder : MonoBehaviour
+{
+
+    [SerializeField] private Card 当前选中的卡片;
+    [SerializeReference] private Card 当前鼠标悬停的卡片;
+
+    [SerializeField] private GameObject 用于存储卡片槽的预制件;
+    private RectTransform RectTransform控制UI元素的位置和大小;
+
+    [Header("Spawn Settings")]
+    [SerializeField] private int 要生成的卡片数量 = 7;
+    public List<Card> 列表字段用于存储所有卡片的引用;
+
+    bool 是否正在交换位置 = false;
+    [SerializeField] private bool 返回时是否使用动画 = true;
+
+    void Start()
+    {
+        for (int i = 0; i < 要生成的卡片数量; i++)
+        {
+            Instantiate(用于存储卡片槽的预制件, transform);
+        }
+
+        RectTransform控制UI元素的位置和大小 = GetComponent<RectTransform>();
+        列表字段用于存储所有卡片的引用 = GetComponentsInChildren<Card>().ToList();
+
+        int cardCount = 0;
+        //初始化一个计数器，用于给卡片命名
+        foreach (Card card in 列表字段用于存储所有卡片的引用)
+        {
+            card.PointerEnterEvent.AddListener(CardPointerEnter);
+            card.PointerExitEvent.AddListener(CardPointerExit);
+            card.BeginDragEvent.AddListener(BeginDrag);
+            card.EndDragEvent.AddListener(EndDrag);
+            card.name = cardCount.ToString();
+            cardCount++;
+        }
+        //为每张卡片添加事件监听器，并给它们命名
+        StartCoroutine(Frame());
+        //开始一个协程，用于更新卡片的视觉索引
+        IEnumerator Frame()
+        {
+            yield return new WaitForSecondsRealtime(.1f);
+            for (int i = 0; i < 列表字段用于存储所有卡片的引用.Count; i++)
+            {
+                if (列表字段用于存储所有卡片的引用[i].卡片视觉处理器 != null)
+                    列表字段用于存储所有卡片的引用[i].卡片视觉处理器.UpdateIndex(transform.childCount);
+            }
+        }
+        //协程Frame，它等待0.1秒，然后更新所有卡片的视觉索引
+    }
+
+    private void BeginDrag(Card card)
+    {
+        当前选中的卡片 = card;
+    }
+    //当卡片开始被拖动时调用的方法。将当前拖动的卡片设置为选中状态。
+
+    void EndDrag(Card card)
+    {
+        if (当前选中的卡片 == null)
+            return;//如果没有任何卡片被选中，就结束方法。
+
+        当前选中的卡片.transform.DOLocalMove(当前选中的卡片.被选择 ? new Vector3(0, 当前选中的卡片.selectionOffset,0) : Vector3.zero, 返回时是否使用动画 ? .15f : 0).SetEase(Ease.OutBack);
+
+        RectTransform控制UI元素的位置和大小.sizeDelta += Vector2.right;
+        RectTransform控制UI元素的位置和大小.sizeDelta -= Vector2.right;
+        //这两行代码看起来是多余的，可能是用于触发布局更新。
+        当前选中的卡片 = null;
+        //将选中的卡片重置为null。
+    }
+
+    void CardPointerEnter(Card card)
+    {
+        当前鼠标悬停的卡片 = card;
+    }
+    //当鼠标指针进入卡片区域时调用的方法,将鼠标悬停的卡片设置为当前卡片。
+    void CardPointerExit(Card card)
+    {
+        当前鼠标悬停的卡片 = null;
+    }
+    //当鼠标指针离开卡片区域时调用的方法。将鼠标悬停的卡片重置为null。
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            if (当前鼠标悬停的卡片 != null)
+            {
+                Destroy(当前鼠标悬停的卡片.transform.parent.gameObject);
+                列表字段用于存储所有卡片的引用.Remove(当前鼠标悬停的卡片);
+
+            }
+        }
+        //如果按下Delete键，并且有卡片被悬停，销毁该卡片的游戏对象，并从列表中移除
+        if (Input.GetMouseButtonDown(1))
+        {
+            foreach (Card card in 列表字段用于存储所有卡片的引用)
+            {
+                card.Deselect();
+            }
+        }//如果按下鼠标右键，取消选择所有卡片
+
+        if (当前选中的卡片 == null)
+            return;
+        //如果没有卡片被选中，就结束Update方法
+        if (是否正在交换位置)
+            return;
+        //如果卡片正在交换位置，就结束Update方法
+        for (int i = 0; i < 列表字段用于存储所有卡片的引用.Count; i++)
+        {//开始一个循环，检查每张卡片。
+
+            if (当前选中的卡片.transform.position.x > 列表字段用于存储所有卡片的引用[i].transform.position.x)
+            {
+                if (当前选中的卡片.ParentIndex() < 列表字段用于存储所有卡片的引用[i].ParentIndex())
+                {
+                    交换两张卡片位置的函数(i);
+                    break;
+                }
+            }//如果选中的卡片在x轴上的位置大于当前卡片，并且它的父索引小于当前卡片的父索引，调用Swap方法交换它们的位置。
+
+            if (当前选中的卡片.transform.position.x < 列表字段用于存储所有卡片的引用[i].transform.position.x)
+            {
+                if (当前选中的卡片.ParentIndex() > 列表字段用于存储所有卡片的引用[i].ParentIndex())
+                {
+                    交换两张卡片位置的函数(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    void 交换两张卡片位置的函数(int index)
+    {
+        是否正在交换位置 = true;
+
+        Transform focusedParent = 当前选中的卡片.transform.parent;
+        Transform crossedParent = 列表字段用于存储所有卡片的引用[index].transform.parent;
+
+        列表字段用于存储所有卡片的引用[index].transform.SetParent(focusedParent);
+        列表字段用于存储所有卡片的引用[index].transform.localPosition = 列表字段用于存储所有卡片的引用[index].被选择 ? new Vector3(0, 列表字段用于存储所有卡片的引用[index].selectionOffset, 0) : Vector3.zero;
+        当前选中的卡片.transform.SetParent(crossedParent);
+
+        是否正在交换位置 = false;
+
+        if (列表字段用于存储所有卡片的引用[index].卡片视觉处理器 == null)
+            return;
+
+        bool swapIsRight = 列表字段用于存储所有卡片的引用[index].ParentIndex() > 当前选中的卡片.ParentIndex();
+        列表字段用于存储所有卡片的引用[index].卡片视觉处理器.Swap(swapIsRight ? -1 : 1);
+
+        //Updated Visual Indexes
+        foreach (Card card in 列表字段用于存储所有卡片的引用)
+        {
+            card.卡片视觉处理器.UpdateIndex(transform.childCount);
+        }
+    }
+
+}
--- Assets/Scripts/UI/MainUIPanel.cs
+++ Assets/Scripts/UI/MainUIPanel.cs
@@ -7,7 +7,7 @@ using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainUIPanel : UIBasePanel
+public class MainUIPanel : UIBasePanel//这行定义了一个名为MainUIPanel的公共类，它继承自UIBasePanel。
 {
     [Header("UI组件")]
     public List<Button> callGuardButtons;
@@ -16,17 +16,20 @@ public class MainUIPanel : UIBasePanel
     public TextMeshProUGUI playerDieNum;
     private int killNum;
     private int dieNum;
+    //这里定义了一些UI组件的引用，包括按钮列表、滑块、文本显示，以及两个私有的整型变量用于记录击杀和死亡次数。
     [Header("事件参数")]
     [SerializeField] private CallGuardEventChannel callGuardEventChannel;
     [SerializeField] private StatChangeEventChannel statChangeEventChannelPlayerHealth;
     [SerializeField] private GuardChangeEventChannel statChangeEventChannelPlayerKill;
     [SerializeField] private StatChangeEventChannel statChangeEventChannelPlayerMana;
+    //这些是序列化的私有字段，用于存储事件通道的引用，这些事件通道可能用于在不同部分的代码之间传递事件。
     public void BtnCallGuard(int index)
     {
         callGuardEventChannel.Broadcast(index,Team.A);
         callGuardEventChannel.Broadcast(index,Team.B);
     }
-
+    //这是一个公共方法，当"调用守卫"按钮被点击时调用。
+    //它通过callGuardEventChannel广播守卫调用事件，为两个队伍（A和B）
     private void Start()
     {
         Init();
@@ -41,7 +44,8 @@ public class MainUIPanel : UIBasePanel
         dieNum = -1;
         OnKillNumChange(Team.B);
         OnKillNumChange(Team.A);
-        
+        //Init方法初始化事件监听器，并将击杀和死亡次数重置为-1。
+        //然后，它调用OnKillNumChange方法来初始化击杀和死亡次数的显示。
     }
 
     private void OnHealthChange(int healthNum, Team team)
@@ -56,10 +60,10 @@ public class MainUIPanel : UIBasePanel
     {
         if(team==Team.A)
             playerManaSlider.value = ManaNum/12f;
-
+        //当A队的法力值变化时，这个方法会更新UI中的滑块值
     }
-    
-    
+
+
     private void OnKillNumChange(Team team)
     {
         if(team==Team.B)
@@ -67,9 +71,9 @@ public class MainUIPanel : UIBasePanel
         if(team==Team.A)
             playerDieNum.text =  "" + ++dieNum;
     }
-
+    //当击杀或死亡次数变化时，这个方法会更新UI中的文本显示。对于B队，它增加击杀次数；对于A队，它增加死亡次数。
     public void BackToStart()
     {
         SceneManager.LoadScene(0);
     }
-}
+}//这是一个公共方法，用于返回到游戏的开始场景。它使用SceneManager来重新加载索引为0的场景。
